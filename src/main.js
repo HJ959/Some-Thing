cd2b105023b7b77a5d7935d29725e72ddaca9457
HJ959 <henryjaames@gmail.com>
2022-03-24 14:45:59 +0000

    working demo

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ scene.add(camera);
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({});
+const renderer = new THREE.WebGLRenderer({canvas:canvas});
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
